11d561587e0bdf3be55db1d509a147b637670e3f
Miguel <jimenezjimenezw@gmail.com>
2018-07-12 05:44:49 -0500

    Subir al sistema

--- Creacion_Daemon/Service1.cs
+++ Creacion_Daemon/Service1.cs
@@ -7,22 +7,45 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Timers;
 
 namespace Creacion_Daemon
 {
     public partial class Service1 : ServiceBase
     {
+        public Timer tiempo;
+        public string fecha;
+        public string NombreArchivo;
         public Service1()
         {
             InitializeComponent();
+            tiempo = new Timer();
+            tiempo.Interval = 10000;
+            tiempo.Elapsed += new ElapsedEventHandler(tiempo_espera);
+            fecha = DateTime.Now.ToShortDateString().Replace(@"/", "-");
         }
 
         protected override void OnStart(string[] args)
         {
+            tiempo.Enabled = true;
         }
 
         protected override void OnStop()
         {
+
+        }
+        public void tiempo_espera(object sender,EventArgs e)
+        {
+            if (!Directory.Exists(@"C:/PruebaOrdenamiento/"+fecha))
+            {
+                Directory.CreateDirectory(@"C:/PruebaOrdenamiento/" + fecha);
+            }
+            foreach(string file in Directory.GetFiles(@"C:/PruebaOrdenamiento/","*.*"))
+            {
+                NombreArchivo = Path.GetFileName(file);
+                File.Move(file, @"C:/PruebaOrdenamiento/" + fecha + "/"+NombreArchivo);
+            }
         }
     }
 }
